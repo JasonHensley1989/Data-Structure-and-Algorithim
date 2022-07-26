14fb07c16f1f2da3c63a51f2e3536742c39f5c7a
Jason Hensley <jasonhensley1989@gmail.com>
2022-07-26 18:51:31 -0400

    golf stroke count algo

--- main.js
+++ main.js
@@ -300,3 +300,30 @@ countCard(2);
 // algorithims for this week plans
 
 // find the square root for three methods
+
+// return stroke count for golf game
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if(strokes === 1) {
+    return 'Hole-in-one!';
+  } else if(strokes <= par - 2) {
+    return 'Eagle';
+  } else if(strokes === par - 1) {
+    return 'Birdie';
+  } else if(strokes === par) {
+    return 'Par';
+  } else if(strokes === par + 1) {
+    return 'Bogey';
+  } else if(strokes === par  + 2){
+    return 'Double Bogey';
+  } else {
+    return 'Go Home!';
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
